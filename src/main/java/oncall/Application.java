60c901d45a78dbf1b68b336e52d9c44dd6a4ba77
Yeongjae Kim <sees77770@gmail.com>
2024-12-13 13:54:29 +0900

    feat: 컨트롤러 추가

--- src/main/java/oncall/Application.java
+++ src/main/java/oncall/Application.java
@@ -1,7 +1,17 @@
 package oncall;
 
+import oncall.controller.OncallController;
+import oncall.view.InputView;
+import oncall.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        OncallController oncallController = new OncallController(
+                inputView, outputView
+        );
+        oncallController.run();
     }
 }
---
+++ src/main/java/oncall/controller/OncallController.java
@@ -0,0 +1,20 @@
+package oncall.controller;
+
+import oncall.view.InputView;
+import oncall.view.OutputView;
+
+public class OncallController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public OncallController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+
+    }
+
+}
