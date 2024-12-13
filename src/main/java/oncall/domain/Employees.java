23881c0e4c1e6997b80b3bfbb9eb0fc0035e9d01
Yeongjae Kim <sees77770@gmail.com>
2024-12-13 14:18:00 +0900

    feat: 근무자 그룹 클래스 추가

---
+++ src/main/java/oncall/domain/Employees.java
@@ -0,0 +1,12 @@
+package oncall.domain;
+
+import java.util.List;
+
+public class Employees {
+
+    private final List<Employee> employees;
+
+    public Employees(List<Employee> employees) {
+        this.employees = employees;
+    }
+}
