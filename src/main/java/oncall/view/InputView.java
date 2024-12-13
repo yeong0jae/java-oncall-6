cc730bdd8c080d132ce13e918be19c08c64ad7c2
Yeongjae Kim <sees77770@gmail.com>
2024-12-13 13:52:33 +0900

    feat: 뷰 기능 추가

---
+++ src/main/java/oncall/view/InputView.java
@@ -0,0 +1,10 @@
+package oncall.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public void read() {
+        Console.readLine();
+    }
+}
---
+++ src/main/java/oncall/view/OutputView.java
@@ -0,0 +1,8 @@
+package oncall.view;
+
+public class OutputView {
+
+    public void print() {
+        System.out.println();
+    }
+}
